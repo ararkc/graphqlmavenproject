65fb680f9e7b8e2b922034743fc3f195329455b1
Rohit <rohitchaubey11aug@gmail.com>
2020-05-06 14:09:53 +0530

    First Commit

---
+++ src/main/java/com/example/graphqlmavenproject/GraphqlmavenprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlmavenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlmavenprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlmavenprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/graphqlmavenproject/GraphqlmavenprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqlmavenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlmavenprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
